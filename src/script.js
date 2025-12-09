89c8e5d346a12a47d23033ea21e1ccca53f6b2e0
AASHRITH <aashrith@Mac.lan>
2025-12-09 22:48:38 +0530

    Animations implemented

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
-// import images as relative image path won't work with vite/vercel.
-import check from "../assets/check.svg";
-import star from "../assets/star.svg";
-import sushi12 from "../assets/sushi-12.png";
-import sushi11 from "../assets/sushi-11.png";
-import sushi10 from "../assets/sushi-10.png";
+// import check from "../assets/check.svg";
+// import star from "../assets/star.svg";
 
-import AOS from "aos";
+const sushi12 = "/sushi-12.png";
+const sushi11 = "/sushi-11.png";
+const sushi10 = "/sushi-10.png";
+
 import "aos/dist/aos.css";
+import AOS from "aos";
 
-// init AOS animation
 AOS.init({
   duration: 1000,
   offset: 100,
